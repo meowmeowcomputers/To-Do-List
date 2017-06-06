d09f546bf3d68d6166406293b3b8d443341dab9d
meowmeowcomputers <ryansimonleon@gmail.com>
2017-06-06 18:18:59 -0500

    Added form back to experiment with

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 var express = require('express');
 var app = express();
 var body_parser = require('body-parser');
-var db = require('pg-promise')
-
-
+var promise = require('bluebird');
+var pgp = require('pg-promise')({
+  promiseLib: promise
+});
 
 app.use(body_parser.urlencoded({extended: false}));
 
@@ -14,4 +15,14 @@ app.get('/', function (request, response) {
 
 app.listen(8002, function () {
   console.log('Listening on port 8002');
+});
+
+app.get('/form', function(request, response) {
+  response.render('form.hbs', {title:'form'});
+});
+app.post('/submit', function (request, response) {
+  console.log(request.body);
+  console.log(request.body.name)
+  var x = request.body.name
+  response.send(`${x}`);
 });
